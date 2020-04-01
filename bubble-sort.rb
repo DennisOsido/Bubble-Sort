5b48a0e07d544ba1448105b73fbf71e3c3c6b44e
Dennis Osido <dennisosido@yahoo.com.ph>
2020-04-01 18:29:34 +0800

    Added Initial Sorting Algorithm

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,15 +1,18 @@
-def bubble_sort(array)
+def bubble_sort (numbers)
 
+  nums = numbers.length
+  flip = false
+
+  while flip == false do 
+    (nums-1).times do |x|
+      if numbers[x] > numbers[x+1]
+        numbers[x], numbers[x+1] = numbers[x+1], numbers[x]
+        flip = true
+      end  
+    end
+  next if flip == true
+  end  
+  numbers		
 end
 
-=begin
-bubble_sort_by{
-    |a, b|
-    yield
-    a <=> b
-}
-=end
-
-bubble_sort_by(["hi","hello","hey"]) do |left,right|
-    left.length - right.length
-end
+p bubble_sort([1, 5, 2, 4, 3, 6])
