17898b586b47465587013235a3b8dcc04de05938
Buyaki01 <50216097+Buyaki01@users.noreply.github.com>
2020-04-03 12:36:44 +0300

    Add to bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,40 +1,26 @@
-def bubble_sort(numbers)
-  nums = numbers.length
-  flip = false
-  while flip == false
-    (nums - 1).times do |x|
-      if numbers[x] > numbers[x + 1]
-        numbers[x], numbers[x + 1] = numbers[x + 1], numbers[x]
-        flip = true
-      end
+def bubble_sort(array)
+  (array.length - 1).times do |i|
+    (array.length - i - 1).times do |a|
+    if array[a] > array[a + 1]
+      array[a], array[a + 1] =  array[a + 1], array[a]
     end
-    next if flip == true
   end
-  numbers
+  end
+  return array
 end
 
-puts 'Unsorted: [1, 5, 2, 4, 3, 6]'
-print 'Sorted: '
-p bubble_sort([1, 5, 2, 4, 3, 6])
-
-def bubble_sort_by(words)
-  word = words.length
-  flip = false
-  while flip == false
-    (word - 1).times do |x|
-      if words[x] < words[x + 1]
-        words[x], words[x + 1] = words[x + 1], words[x]
-        flip = true
-      end
+def bubble_sort_by(array)
+  (array.length - 1).times do |i|
+    (array.length - i - 1).times do |a|
+    if yield(array[a] , array[a + 1]). positive?
+      array[a], array[a + 1] =  array[a + 1], array[a]
     end
-    next if flip == true
   end
-  words
+  end
+  return array
 end
-
-puts
-p bubble_sort_by(%w[hi hello hey])
-
-bubble_sort_by(%w[hi hello hey]) do |left, right|
-  left.length - right.length
+arr=[]
+arr=bubble_sort_by([10,8,5,9,4]) do |x,y|
+  x-y
 end
+print arr
