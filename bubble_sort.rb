66feb04a748498921cf859854c2cd5791090e6aa
Buyaki01 <50216097+Buyaki01@users.noreply.github.com>
2020-04-03 13:06:48 +0300

    Fix rubocop offenses

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,26 +1,22 @@
 def bubble_sort(array)
   (array.length - 1).times do |i|
     (array.length - i - 1).times do |a|
-    if array[a] > array[a + 1]
-      array[a], array[a + 1] =  array[a + 1], array[a]
+      array[a], array[a + 1] = array[a + 1], array[a] if array[a] > array[a + 1]
     end
   end
-  end
-  return array
+  array
 end
 
 def bubble_sort_by(array)
   (array.length - 1).times do |i|
     (array.length - i - 1).times do |a|
-    if yield(array[a] , array[a + 1]). positive?
-      array[a], array[a + 1] =  array[a + 1], array[a]
+      array[a], array[a + 1] = array[a + 1], array[a] if yield(array[a], array[a + 1]). positive?
     end
   end
-  end
-  return array
+  array
 end
-arr=[]
-arr=bubble_sort_by([10,8,5,9,4]) do |x,y|
-  x-y
+
+arr = bubble_sort_by([10, 8, 5, 9, 4]) do |x, y|
+  x - y
 end
 print arr
